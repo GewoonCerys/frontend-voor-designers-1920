8b718fc76880f58611e261fd158fa5cc9ae1f13f
Cerys Hancock <ceryslhancock@gmail.com>
2020-03-26 19:59:15 +0100

    update

--- opdracht3/README.md
+++ opdracht3/README.md
@@ -1,10 +1,9 @@
 # Vragen
 
-- Ik snap even helemaal niet zo goed hoe ik deze api kan bewerken. Ik wil bijvoorbeeld de titels horizontaal inplaats van verticaal. Hoe doe ik dit? Want in de html staat niet als een h1 oid wat ik kan oproepen in de css.
+Ik heb niet veel voortgang, vind dit echt lastig. Ik heb bij anderen hun codepens gekeken (Sebastiaan, Michelino) maar ben niet veel wijzer geworden. Ik snap nu wel hoe ik elementen in css kan stylen, dus bedankt voor de codepen met uitleg!
+Mijn vraag:
 
-- Ik wil er bijvoorbeeld ook een tijdlijn van maken, maar hoe kan ik zo'n soort graphic toevoegen zodat hij onder de juiste namen blijft?
-
-
+Op het moment klik je op een jaartal en krijg je een pop up te zien. Wat ik wil is dat je klikt op een jaartal en dat dan de description van bijbehorend jaartal uit klapt/tevoorschijn komt. Als je opnieuw klikt, klapt deze weer dicht en/of verdwijnt deze weer. Qua CSS heb ik nog niets "mooi gemaakt" omdat ik eerst wil dat de javascript werkt. Wat moet ik doen? Hoe schrijf ik deze functie? Ik probeerde Sebastiaan na te doen maar dat werd juist erg verwarrend. De eventlistener zit volgens mij nu wel op de juiste plek want de alert werkt wel. Maar dit gebeurd als je op ieder jaartal drukt en ik wil eigenlijk dat het per jaartal de description van dat bijbehorende jaartal laat zien.
 
 
 # frontend voor designers - opdracht 3: Een interactie uitwerken met externe data
@@ -34,8 +33,13 @@ Om te kijken of ik nog wist wat er gebeurde heb ik de namen weggehaald en er des
 
 
 ## stap 3: Vormgeven met css
-//
+Hier ga ik me iets later op focussen. Ik wil voor nu dat de javascript enigszins werkt.
  
 
 ## stap 4: Javascript
-//
+![tijdlijn versie 03](img/stap04.jpg "article blocks")
+Door de hulp van Sanne ben ik iets verder gekomen omdat ik nu weet hoe ik kan stylen in css. Wat ik nu wil gaan proberen is dat de jaartallen klikbaar worden. Wanneer een gebruiker hierop klikt, wil ik dat de blauwe blokken pas tevoorschijn komen. Ik heb nu wel een event listener die je een pop up geeft, maar dat is niet wat ik wil. Hierdoor weet ik in principe dat de click werkt, alleen moet de functie dus anders worden.
+
+Ik had gekeken bij Sebastiaans codepen. Hij heeft een click op de gehele article, waardoor je de description ziet. Dit is eigenlijk ook wat ik wil. Ik wil dat als je op de jaartallen klikt, de description en title tevoorschijn komen. Ik probeerde zijn code te begrijpen door mijn code erop toe te passen, maar hierdoor raakte ik juist meer in de war (oeps). Ik snap wel het principe, maar dingen zoals "substring" ken(de) ik niet. Hoe meer ik las hoe meer ik zag dat er steeds meer dingen gebeurde zoals "vulmetbasics" en "vulmetdetails" - snap er gewoon even niks meer van.
+
+Ik vind het erg lastig omdat ik niet gewend ben om te werken met data die je ergens ophaalt. Ik ben gewend dat er in de html elementen staan die ik vervolgens aanpas. Nu alles in de javascript gebeurd vind ik het erg verwarrend en weet ik niet waar ik moet beginnen.
--- opdracht3/scripts/script.js
+++ opdracht3/scripts/script.js
@@ -1,43 +1,87 @@
-const header = document.querySelector('header');
-const section = document.querySelector('section');
+var requestURL = ' https://ghibliapi.herokuapp.com/films';
+var request = new XMLHttpRequest();
+var button = document.querySelector('button');
 
-let requestURL = ' https://ghibliapi.herokuapp.com/films';
-
-let request = new XMLHttpRequest();
 
 request.open('GET', requestURL);
-
 request.responseType = 'json';
 request.send();
 
 request.onload = function() {
-  const movies = request.response;
-  const description = request.response;
-    showmovietitle(movies);
-    showmovieDescription(description);
-    console.log(description);
+  	const movies = request.response;
+	console.log(movies);
+	showMovies(movies);
 }
 
-function showmovietitle(jsonObj) {
-    
-    var i;
-    for (i = 0; i < 20; i++) {
-    
-    const title = document.createElement('p');
-    title.textContent = jsonObj[i]['title'];
-    header.appendChild(title);
+function showMovies(movies) {
+	/* door de array met movies loopen */
+    for (let i = 0; i < 20; i++) {
+		 /* een container om de elementen van een film in te stoppen */
+		var filmDate = document.createElement('button');  
+        let theMovie = document.createElement('article');
+          
+         /* de elementen van een film */
+		 let release_date = document.createElement('p'); 
+         let title = document.createElement('h2');
+		 let description = document.createElement('p');
+        
+		 
+		 /* de elementen van de film vullen */
+		release_date.textContent = movies[i]['release_date']; 
+        title.textContent = movies[i]['title'];
+		 description.textContent = movies[i]['description'];
+         
+        
+        
+		  /* de elementen aan de container toevoegen */
+		filmDate.appendChild(release_date); 
+        theMovie.appendChild(title);
+		 theMovie.appendChild(description);
+         
+        
+		 /* de container aan de main toevoegen */
+         document.body.querySelector("main").appendChild(filmDate);
+		 document.body.querySelector("main").appendChild(theMovie);
+        
+        filmDate.addEventListener('click', function (event) {
+  alert('Ja leuk dit, maar dit moet dus eigenlijk zo zijn dat nu de descriptions te voorschijn komen. De alert is even om te testen, ik wil niet dat het uiteindelijk een alert wordt');
+});
+       
     }
+
 }
 
-function showmovieDescription(jsonObj) {
-    
-    var i;
-    for (i = 0; i < 20; i++) {
-    
-    const description = document.createElement('p');
-    description.textContent = jsonObj[i]['description'];
-    header.appendChild(description);
-    }
-}
+//function vulFilmMetInfo(deFilm) {
+//	/* het nummer van de film bepalen */
+//	let filmID = deFilm.id;
+//	let volgNummer = filmID.substring(20);
+//
+//	
+//	/* de info van alleen die film ophalen */	
+//	let requestURL = ' https://ghibliapi.herokuapp.com/films'+volgNummer+'.json';
+//	let request = new XMLHttpRequest();
+//	request.open('GET', requestURL);
+//
+//	request.responseType = 'json';
+//	request.send();
+//	
+//	/* dit doen als de info van de film binnenkomst */
+//	request.onload = function () {
+//		const filmInfo = request.response;
+//		
+//		/* als de film de class "details" heeft */
+//		if(deFilm.classList.contains("details")){
+//			/* de class "details" verwijderen */
+//			deFilm.classList.remove("details");
+//			/* de film vullen met de basics */
+//			vulMetBasics(deFilm, filmInfo);	
+//		}
+//		/* als de film de class "details" niet heeft */
+//		else {
+//			/* de class "details" toevoegen */
+//			deFilm.classList.add("details");
+//			/* de film vullen met de details */
+//			vulMetDetails(deFilm, filmInfo);	
+//		}
+//	}
 
-showmovietitle.addEventListener('click');
