227a646ea4c71e4fe69f7e6ea6b7842fcee88232
Cerys Hancock <ceryslhancock@gmail.com>
2020-04-08 15:12:49 +0200

    update

--- opdracht3/scripts/script.js
+++ opdracht3/scripts/script.js
@@ -13,7 +13,7 @@ request.onload = function () {
     console.log(movies);
     showMovies(movies);
 }
-
+/* laat de films zien */
 function showMovies(movies) {
     /* door de array met movies loopen */
     for (let i = 0; i < 20; i++) {
@@ -40,15 +40,18 @@ function showMovies(movies) {
         document.body.querySelector("main").appendChild(filmDate);
         document.body.querySelector("main").appendChild(theMovie);
 
+        /* Als er op de datum wordt geklikt, laat dan de info van de bijbehorende film zien */
         filmDate.addEventListener('click', toggleMovieInfo);
     }
 }
 
+/* film laten zien functie */
 function toggleMovieInfo(event) {
     var hetArticleNaDeButton = this.nextElementSibling;
     hetArticleNaDeButton.classList.toggle("show");
 }
 
+/* sortable plugin om een watchlist te maken */
 new Sortable(movieList, {
     group: 'shared', // set both lists to same group
     animation: 150
@@ -59,12 +62,13 @@ new Sortable(watchList, {
     animation: 150
 });
 
-// foto'tje onderaan veranderen
+/* foto'tje onderaan veranderen */
 var achtergrond = document.getElementById('achtergrond');
 var ghibliFotos = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
 var huidigeAchtergrond = 0;
 var maxAchtergrond = ghibliFotos.length;
 
+/* vorige en volgende met de pijltjes toetsen wat loopt */
 function volgendeAchtergrond() {
     huidigeAchtergrond = huidigeAchtergrond + 1;
     if (huidigeAchtergrond >= maxAchtergrond) {
@@ -80,8 +84,7 @@ function vorigeAchtergrond() {
     }
     achtergrond.src = 'img/' + ghibliFotos[huidigeAchtergrond] + '.jpg';
 }
-
-// het indrukken van de pijltjes toetsen. 37 is pijltje links en 39 is pijltje rechts
+/* het indrukken van de pijltjes toetsen. 37 is pijltje links en 39 is pijltje rechts */
 document.onkeydown = function (e) {
     if (e.which == 37) {
         vorigeAchtergrond(e);
